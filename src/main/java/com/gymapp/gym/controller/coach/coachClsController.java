20fa12f6526627c88815a7ddc02a17483edd0bbd
FrancoAndres190 <francoandresligorria190@gmail.com>
2025-06-13 19:08:05 -0300

    FL - Agregando algunos archivos que faltaron

---
+++ src/main/java/com/gymapp/gym/controller/coach/coachClsController.java
@@ -0,0 +1,58 @@
+package com.gymapp.gym.controller.coach;
+
+import com.gymapp.gym.persistence.dtos.Cls.CreateClsDTO;
+import com.gymapp.gym.persistence.entities.Cls;
+import com.gymapp.gym.service.ClsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/coach/classes")
+public class coachClsController {
+
+    @Autowired
+    ClsService clsService;
+
+
+    @GetMapping()
+    public ResponseEntity<List<Cls>> getAll(){
+
+        return ResponseEntity.ok(clsService.getAll());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cls> getOneById(@RequestBody Long id){
+
+        return ResponseEntity.ok(clsService.getOneById(id));
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createCls(@RequestBody CreateClsDTO createClsDTO){
+
+        return ResponseEntity.ok(clsService.createCls(createClsDTO));
+
+    }
+
+
+    //CORREGIR DE ACA PARA ABAJO............
+    @PutMapping()
+    public void editCls(@RequestBody Cls cls) {
+
+        clsService.editCls(cls);
+    }
+
+    @DeleteMapping()
+    public void deleteCls(@RequestBody Cls cls) {
+
+        clsService.deleteCls(cls);
+
+    }
+
+
+}
